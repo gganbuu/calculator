b4a147efdb1c34dc7050a303a7ecc76d4656756b
gganbu <timbuctee@gmail.com>
2026-04-01 10:58:54 +1000

    added button functionality and display updates

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
 // display declaration
-const display = document.querySelector(".calculator.display")
+const display = document.querySelector(".calculator-display");
 
-// button declaration
+// calculator declaration
+const calculator = document.querySelector(".calculator-container");
 
-const deleteBtn = document.querySelector(".calculator-delete")
-const clearAllBtn = document.querySelector(".calculator-clearall")
+// button logic
+calculator.addEventListener("click", (e) => {
+    const target = e.target;
 
-const sevenBtn = document.querySelector(".calculator-7")
-const eightBtn = document.querySelector(".calculator-8")
-const nineBtn = document.querySelector(".calculator-9")
-const fourBtn = document.querySelector(".calculator-4")
-const fiveBtn = document.querySelector(".calculator-5")
-const sixBtn = document.querySelector(".calculator-6")
-const oneBtn = document.querySelector(".calculator-1")
-const twoBtn = document.querySelector(".calculator-2")
-const threeBtn = document.querySelector(".calculator-3")
+    if (!target.classList.contains("btn")) return;
 
-const addBtn = document.querySelector(".calculator-add")
-const subtractBtn = document.querySelector(".calculator-subtract")
-const multiplyBtn = document.querySelector(".calculator-multiply")
-const divideBtn = document.querySelector(".calculator-divide")
-const zeroBtn = document.querySelector(".calculator-0")
-const dotBtn = document.querySelector(".calculator-dot")
-const equalsBtn = document.querySelector(".calculator-equals")
+    const value = target.dataset.value;
+    const action = target.dataset.action;
 
-// event listeners
-// oneBtn.addEventListener("click", (e) => {
-//     display.textContent 
-// })
+    if (value) {
+        display.textContent += value;
+    }
 
+    if (action == "clear") {
+        display.textContent = "";
+    }
+
+    if (action == "delete") {
+        display.textContent = display.textContent.slice(0,display.textContent.length-1);
+    }
+
+
+})
 
 // calculation logic
 const operate = (number1, number2, operator) => {   
@@ -38,7 +36,7 @@ const operate = (number1, number2, operator) => {
         case "-":
             return subtract(number1, number2);
         case "/":
-            return ide(number1, number2);
+            return divide(number1, number2);
         case "*":
             return multiply(number1, number2);
 
@@ -53,7 +51,7 @@ const subtract = (a,b) => {
 	return a-b; 
 };
 
-const ide = (a,b) => {
+const divide = (a,b) => {
 	return a/b; 
 };
 
@@ -61,3 +59,22 @@ const multiply = (a,b) => {
 	return a*b; 
 };
 
+// prevous declarations
+// const deleteBtn = document.querySelector(".calculator-delete")
+// const clearAllBtn = document.querySelector(".calculator-clearall")
+// const sevenBtn = document.querySelector(".calculator-7")
+// const eightBtn = document.querySelector(".calculator-8")
+// const nineBtn = document.querySelector(".calculator-9")
+// const fourBtn = document.querySelector(".calculator-4")
+// const fiveBtn = document.querySelector(".calculator-5")
+// const sixBtn = document.querySelector(".calculator-6")
+// const oneBtn = document.querySelector(".calculator-1")
+// const twoBtn = document.querySelector(".calculator-2")
+// const threeBtn = document.querySelector(".calculator-3")
+// const addBtn = document.querySelector(".calculator-add")
+// const subtractBtn = document.querySelector(".calculator-subtract")
+// const multiplyBtn = document.querySelector(".calculator-multiply")
+// const divideBtn = document.querySelector(".calculator-divide")
+// const zeroBtn = document.querySelector(".calculator-0")
+// const dotBtn = document.querySelector(".calculator-dot")
+// const equalsBtn = document.querySelector(".calculator-equals")
